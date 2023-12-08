5a0b964718e2cc642a72cabb715057d47a697e37
KiKoS0 <22998716+KiKoS0@users.noreply.github.com>
2023-12-08 21:53:16 +0100

    Add sentry error tracking (#144)

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,41 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { resolve } from 'path'
 import { webpackStats } from 'rollup-plugin-webpack-stats'
+import { sentryVitePlugin } from '@sentry/vite-plugin'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [svelte({}), webpackStats()],
-  envDir: './env',
-  build: {
-    rollupOptions: {
-      input: {
-        web: resolve(__dirname, 'video_overlay.html'),
-        mobile: resolve(__dirname, 'mobile.html'),
-        config: resolve(__dirname, 'config.html')
-      },
-      output: {
-        assetFileNames: 'assets/[name].[hash][extname]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js'
+export default defineConfig(({ mode }) => {
+  const productionPlugins =
+    mode === 'production'
+      ? [
+          sentryVitePlugin({
+            org: 'kikos0',
+            project: 'dbd-tooltips',
+            authToken: process.env.SENTRY_AUTH_TOKEN,
+            telemetry: false
+          })
+        ]
+      : []
+
+  return {
+    base: './',
+    plugins: [svelte({}), webpackStats(), [...productionPlugins]],
+    envDir: './env',
+    build: {
+      rollupOptions: {
+        input: {
+          web: resolve(__dirname, 'video_overlay.html'),
+          mobile: resolve(__dirname, 'mobile.html'),
+          config: resolve(__dirname, 'config.html')
+        },
+        output: {
+          assetFileNames: 'assets/[name].[hash][extname]',
+          chunkFileNames: 'assets/[name].[hash].js',
+          entryFileNames: 'assets/[name].[hash].js',
+          manualChunks: {
+            sentry: ['@sentry/svelte']
+          }
+        }
       }
     }
   }
