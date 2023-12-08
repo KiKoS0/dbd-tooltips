33b53d832c14ab404045220ba2d558bc70b38dc4
Riadh <22998716+KiKoS0@users.noreply.github.com>
2023-12-08 21:35:36 +0100

    Add sentry error tracking

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import './app.css'
 import App from './App.svelte'
 
+import { init } from '@sentry/svelte'
+
+init({
+  dsn: 'https://cf4e5e31d67efd3fee13291158e779c6@o963300.ingest.sentry.io/4506361992314880',
+  integrations: []
+})
+
 const app = new App({ target: document.getElementById('app') as HTMLElement })
 
 export default app
