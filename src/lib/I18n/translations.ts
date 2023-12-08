f8610ffa8e50ddc8665d702bf51cd89a08142603
KiKoS0 <22998716+KiKoS0@users.noreply.github.com>
2023-12-08 19:30:05 +0100

    Implement addons on the mobile version (#143)

--- src/lib/I18n/translations.ts
+++ src/lib/I18n/translations.ts
@@ -9,7 +9,9 @@ const translations: Translations = {
     'show.addons': 'Addons are available',
     'show.perks': 'You can hover over the perks',
     'perk.tooltip.subtitle': '{{actor}} PERK',
-    general: 'GENERAL'
+    general: 'GENERAL',
+    loadout: 'Current loadout',
+    support: 'Support us'
   },
   de: {
     'show.more': 'Klicken Sie, um mehr zu erfahren',
@@ -17,7 +19,9 @@ const translations: Translations = {
     'show.addons': 'Addons sind verfügbar',
     'show.perks': 'Sie können mit dem Mauszeiger über die Talente fahren',
     'perk.tooltip.subtitle': '{{actor}} TALENT',
-    general: 'ALLGEMEINE'
+    general: 'ALLGEMEINE',
+    loadout: 'Aktuelle Auslastung',
+    support: 'Unterstütze uns'
   },
   fr: {
     'show.more': 'Cliquez pour voir les changelogs du perk.',
@@ -25,7 +29,9 @@ const translations: Translations = {
     'show.addons': 'Les addons sont disponibles',
     'show.perks': 'Vous pouvez survoler les compétences',
     'perk.tooltip.subtitle': 'COMPÉTENCE {{actor}}',
-    general: 'GÉNÉRALE'
+    general: 'GÉNÉRALE',
+    loadout: 'Compétences actuelles',
+    support: 'Soutenez-nous'
   }
 }
 
