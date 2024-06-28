0eb59de24639a07552e4ecd0eb88157c39714f6e
Riadh <22998716+KiKoS0@users.noreply.github.com>
2024-06-28 02:19:23 +0200

    chore: Update dev config

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
+import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
 
 import { init } from '@sentry/svelte'
+import { browserTracingIntegration } from '@sentry/svelte'
 
 init({
   dsn: 'https://cf4e5e31d67efd3fee13291158e779c6@o963300.ingest.sentry.io/4506361992314880',
-  integrations: []
+  integrations: [browserTracingIntegration()],
+  tracesSampleRate: 0.1
 })
 
-const app = new App({ target: document.getElementById('app') as HTMLElement })
-
+const app = mount(App, { target: document.getElementById('app')! })
 export default app
