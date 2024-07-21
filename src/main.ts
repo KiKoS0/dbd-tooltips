d9a3c2c24e375e52cc98521771f2c167923eaf02
Riadh <22998716+KiKoS0@users.noreply.github.com>
2024-07-21 19:06:48 +0200

    chore: Activate sentry profiling

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,18 @@ import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
 
-import { init } from '@sentry/svelte'
+import {
+  init,
+  browserTracingIntegration,
+  browserProfilingIntegration
+} from '@sentry/svelte'
 
 init({
   dsn: 'https://cf4e5e31d67efd3fee13291158e779c6@o963300.ingest.sentry.io/4506361992314880',
-  integrations: [],
-  tracesSampleRate: 0.1
+  integrations: [browserTracingIntegration(), browserProfilingIntegration()],
+  tracePropagationTargets: ['localhost', /^https:\/\/api\.dbdtwitch\.com\/api/],
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0
 })
 
 const app = mount(App, { target: document.getElementById('app')! })
