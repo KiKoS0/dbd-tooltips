52fe9acc4263ef826d54ad6e8ac09794bab6918f
Riadh <22998716+KiKoS0@users.noreply.github.com>
2024-07-22 02:21:27 +0200

    chore: .svelte -> .svelte.ts

---
+++ src/lib/Stores/AppStateStore.svelte.ts
@@ -0,0 +1,28 @@
+let appEnabled = $state(false)
+let isMobile = $state(false)
+let isConfig = $state(false)
+
+export const appStateStore = () => {
+  return {
+    get enabled() {
+      return appEnabled
+    },
+    get isMobile() {
+      return isMobile
+    },
+    get isConfig() {
+      return isConfig
+    },
+    enable: () => (appEnabled = true),
+    disable: () => (appEnabled = false),
+    setAppMode: (mode: 'mobile' | 'config') => {
+      if (mode === 'mobile') {
+        isMobile = true
+        isConfig = false
+      } else {
+        isMobile = false
+        isConfig = true
+      }
+    }
+  }
+}
