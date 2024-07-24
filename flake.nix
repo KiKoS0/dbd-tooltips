1b6b2c25722b7ab80babee5f92d146cbb8e835b0
Riadh <22998716+KiKoS0@users.noreply.github.com>
2024-07-24 03:01:01 +0200

    chore: Add nix flake setup

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Client for the Dead by Daylight Tooltips on Twitch";
+
+  # Flake inputs
+  inputs = {
+    flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/*.tar.gz";
+
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  };
+
+  # Flake outputs that other flakes can use
+  outputs = { self, flake-schemas, nixpkgs }:
+    let
+      # Helpers for producing system-specific outputs
+      supportedSystems = [ "x86_64-linux" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in {
+      # Schemas tell Nix about the structure of your flake's outputs
+      schemas = flake-schemas.schemas;
+
+      # Development environments
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          # Pinned packages available in the environment
+          packages = with pkgs; [
+            nodejs_22
+            nodePackages.yarn
+            nixpkgs-fmt
+          ];
+        };
+      });
+    };
+}
