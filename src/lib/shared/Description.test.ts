d4a139cf91c4924266a258d1b5a254b64a4614a5
Riadh <22998716+KiKoS0@users.noreply.github.com>
2024-07-25 18:31:55 +0200

    chore: Refactor tests under describe block

--- src/lib/shared/Description.test.ts
+++ src/lib/shared/Description.test.ts
@@ -1,37 +1,39 @@
 import { render, screen, waitFor } from '@testing-library/svelte'
-import { expect, test } from 'vitest'
+import { describe, expect, it } from 'vitest'
 
 import Description from './Description.svelte'
-import perks from '../../../test/fixtures/survivors.json'
+import perks from '../../../tests/fixtures/survivors.json'
 import { visualStore } from '../Stores/VisualStore.svelte'
 
 const perk = perks['Adrenaline']
 
-test('renders correctly', () => {
-  const expected =
-    'You are fuelled by unexpecting energy when on the verge of escape'
+describe('Description', () => {
+  it('renders correctly', () => {
+    const expected =
+      'You are fuelled by unexpecting energy when on the verge of escape'
 
-  render(Description, { ...perk })
-  expect(screen.getByText(expected)).toBeInTheDocument()
-})
+    render(Description, { ...perk })
+    expect(screen.getByText(expected)).toBeInTheDocument()
+  })
 
-test('rewrites icon url to CDN`s', () => {
-  render(Description, { ...perk })
+  it('rewrites icon url to CDN`s', () => {
+    render(Description, { ...perk })
 
-  const img = screen.getByRole<HTMLImageElement>('img')
+    const img = screen.getByRole<HTMLImageElement>('img')
 
-  expect(img.src).toBe(
-    `https://${import.meta.env.VITE_CDN_HOST}/images/icons/IconHelp_addons.png`
-  )
-})
+    expect(img.src).toBe(
+      `https://${import.meta.env.VITE_CDN_HOST}/images/icons/IconHelp_addons.png`
+    )
+  })
 
-test('shows changelogs when triggered', async () => {
-  render(Description, { ...perk })
-  expect(screen.queryByText('Patch 7.7.0')).toBeNull()
+  it('shows changelogs when triggered', async () => {
+    render(Description, { ...perk })
+    expect(screen.queryByText('Patch 7.7.0')).toBeNull()
 
-  visualStore().toggleChangelogs()
+    visualStore().toggleChangelogs()
 
-  await waitFor(async () => {
-    expect(await screen.findByText('Patch 7.7.0')).toBeInTheDocument()
+    await waitFor(async () => {
+      expect(await screen.findByText('Patch 7.7.0')).toBeInTheDocument()
+    })
   })
 })
