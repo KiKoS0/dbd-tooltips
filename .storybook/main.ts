7ea7faa19362da99f6413aaa8fd09f2ccf0b9c8d
Riadh <22998716+KiKoS0@users.noreply.github.com>
2025-10-05 00:12:49 +0200

    chore: bump storybook and chromatic

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,15 +3,9 @@ import type { StorybookConfig } from '@storybook/svelte-vite'
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|ts|svelte)'],
   addons: [
-    {
-      name: '@storybook/addon-essentials',
-      options: {
-        docs: false
-      }
-    },
     '@storybook/addon-svelte-csf',
     '@chromatic-com/storybook',
-    '@storybook/experimental-addon-test'
+    '@storybook/addon-vitest'
   ],
   framework: {
     name: '@storybook/svelte-vite',
